da78863546c6e7ebf819d0844a11cdfe683c952e
Hamman Ibrahim Khalil <ibrahimhamman215@gmail.com>
2022-11-15 21:16:41 +0100

    Update useUser.js

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -8,13 +8,12 @@ export default function useUser(uid) {
     async function getUserObjByUserId(uid) {
       const user = await getUserByUid(uid);
       setActiveUser(user || {});
-      // console.log(user);
+     
     }
     if (uid) {
       getUserObjByUserId(uid);
     }
   }, [uid]);
 
-  // console.log('inUseUser', activeUser);
   return { userData: activeUser, setActiveUser };
 }
