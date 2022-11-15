1967338d026a30be40f1ebe6b08e16d30a55dc75
Hamman Ibrahim Khalil <ibrahimhamman215@gmail.com>
2022-11-15 22:25:50 +0100

    Update Sidebar.js

--- src/components/Dashboard/Sidebar.js
+++ src/components/Dashboard/Sidebar.js
@@ -1,9 +1,8 @@
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
 import UserDataContext from '../../context/userdata'
-import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
+import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
 import { getSuggestedProfiles } from "../../hooks/FirestoreServices"
-import { useState } from "react"
 import SuggestedProfiles from "./SuggestedProfiles"
 
 
@@ -16,14 +15,13 @@ export default function Sidebar({ viewProfiles }) {
     async function getUsersNotFollowing() {
       const users = await getSuggestedProfiles(userData)
       setProfiles(users)
-      // console.log(profiles);
     }
     if (userData) {
       getUsersNotFollowing()
     }
   }, [userData])
 
-  // console.log(viewProfiles);
+
   return (<div className={viewProfiles ?'none': ''}>
     {!profiles ? (<div className="sidebarSkel"><Skeleton className="sp" height={`100%`} count={3} /></div>) : profiles.length > 0 && (<>
       <div className="sidebar">
