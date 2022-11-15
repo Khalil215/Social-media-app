a82ad9306ca81f70dde94338e1bdc05cd02e4390
Hamman Ibrahim Khalil <ibrahimhamman215@gmail.com>
2022-11-15 21:12:06 +0100

    Update MainPage.js

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,11 +1,10 @@
 import '../../styles/dashboards.css'
 import Header from '../Header'
 import Body from '../Body'
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect} from 'react'
 import UserContext from '../../context/user'
 import UserDataContext from '../../context/userdata'
 import { useNavigate } from 'react-router-dom'
-// import { userColRef, query, where, onSnapshot } from '../../libraries/firebase'
 import useUser from '../../hooks/useUser'
 
 
@@ -15,9 +14,6 @@ export default function Dashboard() {
   const { User } = useContext(UserContext)
 
   const { userData, setActiveUser } = useUser(User.uid)
-  // if (User) {
-  //   console.log('inMainpage', userData);
-  // }
 
 
   useEffect(() => {
